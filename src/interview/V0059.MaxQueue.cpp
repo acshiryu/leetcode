2ff7422835c21ef869370edcd4f1b7179bd44cd1
hzl <acshiryu@vip.qq.com>
2020-09-02 14:03:36 +0800

    LEETCODE

--- src/interview/V0059.MaxQueue.cpp
+++ src/interview/V0059.MaxQueue.cpp
@@ -6,58 +6,53 @@
 
 class MaxQueue
 {
- public:
-  MaxQueue()
-  {
+  public:
+    MaxQueue() {}
 
-  }
+    int max_value() { return deque_.empty() ? -1 : deque_.front(); }
 
-  int max_value()
-  {
-    return deque_.empty() ? -1 : deque_.front();
-  }
-
-  void push_back(int value)
-  {
-    queue_.push(value);
-    while (!deque_.empty() && value > deque_.back() ){
-      deque_.pop_back();
-    }
-    deque_.push_back(value);
-  }
-
-  int pop_front()
-  {
-    if (queue_.empty()) {
-      return -1;
-    }
-
-    auto front = queue_.front();
-    queue_.pop();
-
-    if (front == deque_.front())
+    void push_back(int value)
     {
-      deque_.pop_front();
+        queue_.push(value);
+        while (!deque_.empty() && value > deque_.back())
+        {
+            deque_.pop_back();
+        }
+        deque_.push_back(value);
     }
 
-    return front;
-  }
+    int pop_front()
+    {
+        if (queue_.empty())
+        {
+            return -1;
+        }
 
- private:
-  deque<int> deque_;
-  queue<int> queue_;
+        auto front = queue_.front();
+        queue_.pop();
+
+        if (front == deque_.front())
+        {
+            deque_.pop_front();
+        }
+
+        return front;
+    }
+
+  private:
+    deque<int> deque_;
+    queue<int> queue_;
 };
 
 int32_t main()
 {
-  MaxQueue *obj = new MaxQueue();
-  int param_1 = obj->max_value();
-  obj->push_back(1000);
-  int param_3 = obj->pop_front();
+    MaxQueue* obj = new MaxQueue();
+    int param_1   = obj->max_value();
+    obj->push_back(1000);
+    int param_3 = obj->pop_front();
 
-  dbg(param_1);
-  dbg(param_3);
+    dbg(param_1);
+    dbg(param_3);
 
-  return 0;
+    return 0;
 }
-
---
+++ src/lcof/L0020.BiaoShiShuZhiDeZiFuChuan.cpp
@@ -0,0 +1,10 @@
+//
+// Created by hzl on 2020/9/2.
+//
+#include <include/cplusplus.h>
+
+class Solution
+{
+  public:
+    bool isNumber(string s) {}
+};
